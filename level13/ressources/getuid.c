95698e4a21ac047807f0cc1c32bd9a5948d1adfa
yoanngi <yoann.ginet@gmail.com>
2020-10-06 13:05:38 +0200

    Update getuid.c

--- level13/ressources/getuid.c
+++ level13/ressources/getuid.c
@@ -11,15 +11,3 @@ uid_t	geteuid(void)
 	write(2, "geteuid\n", 8);
 	return 0x1092;
 }
-
-uid_t	getgid(void)
-{
-	write(2, "getgid\n", 7);
-	return 0x1092;
-}
-
-uid_t	getegid(void)
-{
-	write(2, "getegid\n", 8);
-	return 0x1092;
-}
